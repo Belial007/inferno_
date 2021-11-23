5c22f31bf359b21c312b831e8a52196da53c922b
SAMOKHVALOVA MARIA <92328396+Belial007@users.noreply.github.com>
2021-11-23 19:52:41 +0000

    Задание 1-2

--- 1-2.cpp
+++ 1-2.cpp
@@ -5,21 +5,21 @@
 
 using namespace std;
 
-/ * *
-* \ short Математическая функция, рассчитывающая площадь прямоугольника
-* \ param  - длина прямоугольника, имеющая тип данных с плавающей точкой двойной точности
-* \ param  - коэффициент, имеющий значение тип данных с плавающей точкой двойной точности
-* \ return площадь прямоугольника 
-* * /
-double gets(const double a, const double n);
+/**
+* \brief Математическая функция, рассчитывающая площадь прямоугольника
+* \param const double a - длина прямоугольника, имеющая тип данных с плавающей точкой двойной точности
+* \param const double n - коэффициент, имеющий тип данных с плавающей точкой двойной точности
+* \return Возвращает площадь чисел
+**/
+double getSquare(const double a, const double n);
 
 /**
 * \brief Математическая функция, рассчитывающая периметр прямоугольника
-* \param - длина прямоугольника, имеющая тип данных с плавающей точкой двойной точности
-* \param - коэффициент, имеющий тип данных с плавающей точкой двойной точности
+* \param длина прямоугольника, имеющая тип данных с плавающей точкой двойной точности
+* \param коэффициент, имеющий тип данных с плавающей точкой двойной точности
 * \return периметр прямоугольника 
 **/
-double getp(const double a, const double n);
+double getPerimeter(const double a, const double n);
 
 
 /**
@@ -32,5 +32,11 @@ int main() {
   cout << "Введите длину и коэффициент и нажмите Enter: \n";
   cin >> a >> n;
 
-	const auto s = gets(a, n);
-	const auto p = getp(a, n);
+	const auto square = getSquare(a, n);
+	const auto perimeter = getPerimeter(a, n);
+  cout << "Стророна a = " << a << "\nКоэффициент n = " << n << "\nПериметр = " << square << "\nПлощадь = " << perimeter;
+	return 0;
+}
+  
+double getSquare(const double a, const double n) { return pow(a, 2) * n; } 
+double getPerimeter(const double a, const double n) { return 2 * a * (1 + n); }
